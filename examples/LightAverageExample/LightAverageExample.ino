3ef069fb20a7ed259dafee23f20bfc3dd6a30622
Marcial Fernandez <marcial@larces.uece.br>
2017-01-08 16:25:42 -0300

    Update LightAverageExample.ino

--- examples/LightAverageExample/LightAverageExample.ino
+++ examples/LightAverageExample/LightAverageExample.ino
@@ -1,6 +1,23 @@
-
+/*
+  LightAverageExample.ino - Arduino Sketch example for LightAverage
+  
+  Copyright (c) Marcial Fernandez 2016
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
  
-// include the ThermoAverage library
+// include the LightAverage library
 #include <LightAverage.h>
 
 // create a LightAverage instance, reading from analog pin A0
@@ -14,7 +31,7 @@ LightAverage lightAverage(A0, 10000);
                       1000,            // the LDR dark resistence (0 Lux) in Kohm
                       0.7)             // the LDR gama coeficiente
 */
-// If you have  LDR cdatasheet LDR gama is log(R_10lux/R_100lux)
+// If you have the LDR datasheet LDR gama = log(R_10lux/R_100lux), R in Kohm
 
 void setup() {
   // set up UART
